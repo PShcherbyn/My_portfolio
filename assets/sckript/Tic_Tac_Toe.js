313d7b8354ab56825c7f2c96ea87758458a0d5fd
PavloShcherbyna <Dprogfull@gmail.com>
2023-03-27 20:01:46 +0300

    fitst commit

--- assets/sckript/Tic_Tac_Toe.js
+++ assets/sckript/Tic_Tac_Toe.js
@@ -1,6 +1,18 @@
 var divSquare = '<div id="s$coord" class="square $color"></div>';
+var count = 0;
+let res = [];
 $(function(){
     addSquares();
+    let array = [...$('.square')];
+    console.log($('.square'));
+    let len = array.length;
+    let n = 3; // Предполагая, что каждая строка показывает 4
+    let lineNum = len % n === 0 ? len / n : Math.floor( (len / n) + 1 );
+    for (let i = 0; i < lineNum; i++) {
+        // Метод slice () возвращает мелкую копию части выбранного массива от начала до конца (не включая конец) в новый объект массива. И исходный массив не будет изменен.
+        let temp = array.slice(i*n, i*n+n);
+        res.push(temp);
+    }
 });
 function addSquares (){
     console.log('addSquares');
@@ -12,3 +24,78 @@ function addSquares (){
 function isBlackSquareAt(coord){
     return(coord % 3 + Math.floor (coord/3))%2;
 }
+
+ 
+$('body').on('click', '.square', function(){
+    if($(this) .html()){
+        console.log('This square have symbol')
+        count --;
+    }else if(count %2){
+        $(this).html('O');
+    }else $(this).html('X');
+    console.log(this, count);
+    count++;
+    isWin();
+});
+function isWin(){
+    for (let i=0; i<3; i++){ //перевірка строк
+        let firstSymbol = $(res[i][0]).html();
+        if(firstSymbol == ''){
+            continue;
+        }
+        let win = true;
+        for (let j=1; j<3; j++){
+
+            if ($(res[i][j]).html() != firstSymbol){
+                win = false;
+            }
+        }
+    
+        if (win == true){
+            console.log(firstSymbol + ' wins');
+            return 0;
+        }
+    }
+    for (let i=0; i<3; i++){ //перевірка рядків
+        let firstSymbol = $(res[0][i]).html();
+        if(firstSymbol == ''){
+            continue;
+        }
+        let win = true;
+        for (let j=1; j<3; j++){
+
+            if ($(res[j][i]).html() != firstSymbol){
+                win = false;
+            }
+        }
+    
+        if (win == true){
+            console.log(firstSymbol + ' wins');
+            return 0;
+        }
+    }
+
+    let win = true; //діагональ
+    for(let i=1; i<3; i++){
+        if ($(res[i][i]).html() != $(res[0][0]).html()){
+            win = false;
+        }
+    }
+    if (win == true){
+        console.log($(res[0][0]).html() + ' wins');
+        return 0;
+    }
+    win = true;
+    for(let i=1; i<3; i++){
+        if ($(res[i][2-i]).html() != $(res[0][2]).html()){
+            win = false;
+        }
+    }
+    if (win == true){
+        console.log($(res[0][2]).html() + ' wins');
+        return 0;
+    }
+}
+
+    let i = 10;
+
